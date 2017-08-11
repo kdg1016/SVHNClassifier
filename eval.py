03795b94f529fd21bd08f208ae9a3063a295c6d0
ML07-PC\ML07 <motive@mindslab.ai>
2017-08-11 13:01:07 +0900

    사전 학습된 모델의 checkpoint 및 log 데이터를 이용하여 성능을 평가하는 루틴.

--- eval.py
+++ eval.py
@@ -1,12 +1,12 @@
+#-*- coding: utf-8 -*-
 import os
 import tensorflow as tf
 from meta import Meta
 from evaluator import Evaluator
+import sys
+import argparse
 
-tf.app.flags.DEFINE_string('data_dir', './data', 'Directory to read TFRecords files')
-tf.app.flags.DEFINE_string('checkpoint_dir', './logs/train', 'Directory to read checkpoint files')
-tf.app.flags.DEFINE_string('eval_logdir', './logs/eval', 'Directory to write evaluation logs')
-FLAGS = tf.app.flags.FLAGS
+
 
 
 def _eval(path_to_checkpoint_dir, path_to_eval_tfrecords_file, num_eval_examples, path_to_eval_log_dir):
@@ -24,24 +24,37 @@ def _eval(path_to_checkpoint_dir, path_to_eval_tfrecords_file, num_eval_examples
         print 'Evaluate %s on %s, accuracy = %f' % (path_to_checkpoint, path_to_eval_tfrecords_file, accuracy)
 
 
-def main(_):
-    path_to_train_tfrecords_file = os.path.join(FLAGS.data_dir, 'train.tfrecords')
-    path_to_val_tfrecords_file = os.path.join(FLAGS.data_dir, 'val.tfrecords')
-    path_to_test_tfrecords_file = os.path.join(FLAGS.data_dir, 'test.tfrecords')
-    path_to_tfrecords_meta_file = os.path.join(FLAGS.data_dir, 'meta.json')
-    path_to_checkpoint_dir = FLAGS.checkpoint_dir
+def main_eval(_):
 
-    path_to_train_eval_log_dir = os.path.join(FLAGS.eval_logdir, 'train')
-    path_to_val_eval_log_dir = os.path.join(FLAGS.eval_logdir, 'val')
-    path_to_test_eval_log_dir = os.path.join(FLAGS.eval_logdir, 'test')
+    parser = argparse.ArgumentParser(description="Evaluation Routine for SVHNClassifier")
+    parser.add_argument("--data_dir", required=True, help="Directory to read TFRecords files")
+    parser.add_argument("--path_to_checkpoint_dir", required=True, help="Directory to read checkpoint files")
+    parser.add_argument("--eval_logdir", required=True, help="Directory to write evaluation logs")
+    parser.add_argument("--path_to_train_tfrecords_file", required=True, help="Tfrecords file in train directory")
+    parser.add_argument("--path_to_val_tfrecords_file", required=True, help="Tfrecords file in val directory")
+    parser.add_argument("--path_to_test_tfrecords_file", required=True, help="Tfrecords file in test directory")
+    parser.add_argument("--path_to_tfrecords_meta_file", required=True, help="Meta file in directory")
+    parser.add_argument("--path_to_train_eval_log_dir", required=True, help="Training and evaluating log directory")
+    parser.add_argument("--path_to_val_eval_log_dir", required=True, help="Validating and evaluating log directory")
+    parser.add_argument("--path_to_test_eval_log_dir", required=True, help="Testing and evaluating log directory")
+    args = parser.parse_args()
 
     meta = Meta()
-    meta.load(path_to_tfrecords_meta_file)
+    meta.load(args.path_to_tfrecords_meta_file)
 
-    _eval(path_to_checkpoint_dir, path_to_train_tfrecords_file, meta.num_train_examples, path_to_train_eval_log_dir)
-    _eval(path_to_checkpoint_dir, path_to_val_tfrecords_file, meta.num_val_examples, path_to_val_eval_log_dir)
-    _eval(path_to_checkpoint_dir, path_to_test_tfrecords_file, meta.num_test_examples, path_to_test_eval_log_dir)
+    _eval(args.path_to_checkpoint_dir, args.path_to_train_tfrecords_file, meta.num_train_examples, args.path_to_train_eval_log_dir)
+    _eval(args.path_to_checkpoint_dir, args.path_to_val_tfrecords_file, meta.num_val_examples, args.path_to_val_eval_log_dir)
+    _eval(args.path_to_checkpoint_dir, args.path_to_test_tfrecords_file, meta.num_test_examples, args.path_to_test_eval_log_dir)
 
 
 if __name__ == '__main__':
-    tf.app.run(main=main)
+
+    if len(sys.argv) == 1:
+
+        sys.argv.extend(["--data_dir",  "./data",                                           "--path_to_checkpoint_dir", "./logs/train",
+                         "--eval_logdir", "./logs/eval",                                    "--path_to_train_tfrecords_file", "./data/train.tfrecords",
+                         "--path_to_val_tfrecords_file", "./data/val.tfrecords",            "--path_to_test_tfrecords_file", "./data/test.tfrecords",
+                         "--path_to_tfrecords_meta_file", "./data/meta.json",               "--path_to_train_eval_log_dir", "./logs/eval/train",
+                         "--path_to_val_eval_log_dir", "./logs/eval/val",                   "--path_to_test_eval_log_dir", "./logs/eval/test"])
+
+    tf.app.run(main=main_eval)
