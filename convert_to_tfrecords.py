9fd6f2a4d31dbfc3bcb537fcda1e1aef1a2f9ea2
ML07-PC\ML07 <motive@mindslab.ai>
2017-08-11 12:58:58 +0900

    train / extra / test / user_train 폴더에서 불러온 이미지 추출 좌표가 포함된 .mat파일과 .csv파일을 이용하여 이미지를 crop한 뒤, tensorflow 포맷 형식인 tfrecords로 변환하여 저장하는 루틴.

--- convert_to_tfrecords.py
+++ convert_to_tfrecords.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 import os
 import numpy as np
 import h5py
@@ -6,9 +8,13 @@ from PIL import Image
 import tensorflow as tf
 from meta import Meta
 
-tf.app.flags.DEFINE_string('data_dir', './data',
-                           'Directory to SVHN (format 1) folders and write the converted files')
-FLAGS = tf.app.flags.FLAGS
+import csv
+import sys
+import argparse
+
+
+
+
 
 
 class ExampleReader(object):
@@ -17,20 +23,125 @@ class ExampleReader(object):
         self._num_examples = len(self._path_to_image_files)
         self._example_pointer = 0
 
+    
     @staticmethod
-    def _get_attrs(digit_struct_mat_file, index):
+    def _get_csv_writer(filename, rows, delimiter):
+        with open(filename, 'w') as csvfile:
+            fieldnames = rows[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=delimiter)
+            writer.writeheader()
+            for row in rows:
+                try:
+                    writer.writerow(row)
+                except Exception as detail:
+                    print type(detail)
+                    print detail
+
+    @staticmethod
+    def _get_csv_reader(filename, delimiter):
+        reader = []
+        if not os.path.isfile(filename):
+            csvfile = open(filename, "w")
+        else:
+            csvfile = open(filename, "rb")
+            reader = csv.DictReader(csvfile, delimiter=delimiter)
+        return list(reader)
+
+
+
+
+
+    @staticmethod
+    def _get_attrs(digit_struct_mat_file,index):
         """
         Returns a dictionary which contains keys: label, left, top, width and height, each key has multiple values.
         """
         attrs = {}
         f = digit_struct_mat_file
+
+
         item = f['digitStruct']['bbox'][index].item()
         for key in ['label', 'left', 'top', 'width', 'height']:
             attr = f[item][key]
-            values = [f[attr.value[i].item()].value[0][0]
-                      for i in range(len(attr))] if len(attr) > 1 else [attr.value[0][0]]
+            values = [f[attr.value[i].item()].value[0][0] for i in range(len(attr))] if len(attr) > 1 else [attr.value[0][0]]
+            '''
+            values = []
+            if len(attr) > 1:
+                for i in range(len(attr)):
+                    values.append(f[attr.value[i].item()].value[0][0])
+            else:
+                values = [attr.value[0][0]]
+            '''
+
             attrs[key] = values
         return attrs
+
+    csvList = {}
+
+    @staticmethod
+    def _get_attrs_for_csv(digit_struct_csv_file, index, csvList):
+        """
+        Returns a dictionary which contains keys: label, left, top, width and height, each key has multiple values.
+        """
+        attrs = {}
+        values = []
+
+
+        attr_length = int(csvList[index]['length'])
+
+
+        # 사전 key 값 로드
+        for key in ['height', 'label', 'left', 'top', 'width']:
+
+            attr = key # 해당 되는 속성 지정
+
+            if (attr=='height'):
+                if attr_length  > 1:
+                    for i in range(attr_length):
+                        attr_key = "%s%s%d%s" % (attr, '(', i+1, ')')
+                        values.append(csvList[index][attr_key])
+                else:
+                    values = values
+
+            elif (attr=='label'):
+                if attr_length  > 1:
+                    for i in range(attr_length):
+                        attr_key = "%s%s%d%s" % (attr, '(', i+1, ')')
+                        values.append(csvList[index][attr_key])
+                else:
+                    values = values
+
+            elif (attr=='left'):
+                if attr_length  > 1:
+                    for i in range(attr_length):
+                        attr_key = "%s%s%d%s" % (attr, '(', i+1, ')')
+                        values.append(csvList[index][attr_key])
+                else:
+                    values = values
+
+            elif (attr=='top'):
+                if attr_length  > 1:
+                    for i in range(attr_length):
+                        attr_key = "%s%s%d%s" % (attr, '(', i+1, ')')
+                        values.append(csvList[index][attr_key])
+                else:
+                    values = values
+
+            else :
+                if attr_length  > 1:
+                    for i in range(attr_length):
+                        attr_key = "%s%s%d%s" % (attr, '(', i+1, ')')
+                        values.append(csvList[index][attr_key])
+                else:
+                    values = values
+
+            attrs[key] = values
+            values = []
+
+        return attrs
+
+
+
 
     @staticmethod
     def _preprocess(image, bbox_left, bbox_top, bbox_width, bbox_height):
@@ -75,6 +186,7 @@ class ExampleReader(object):
         for idx, label_of_digit in enumerate(label_of_digits):
             digits[idx] = int(label_of_digit if label_of_digit != 10 else 0)    # label 10 is essentially digit zero
 
+
         attrs_left, attrs_top, attrs_width, attrs_height = map(lambda x: [int(i) for i in x], [attrs['left'], attrs['top'], attrs['width'], attrs['height']])
         min_left, min_top, max_right, max_bottom = (min(attrs_left),
                                                     min(attrs_top),
@@ -88,6 +200,53 @@ class ExampleReader(object):
                                                         max_side,
                                                         max_side)
         image = np.array(ExampleReader._preprocess(Image.open(path_to_image_file), bbox_left, bbox_top, bbox_width, bbox_height)).tobytes()
+        
+
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'image': ExampleReader._bytes_feature(image),
+            'length': ExampleReader._int64_feature(length),
+            'digits': tf.train.Feature(int64_list=tf.train.Int64List(value=digits))
+        }))
+        return example
+
+    ##
+    def read_and_convert_for_csv(self, digit_struct_csv_file, csvList):
+        """
+        Read and convert to example, returns None if no data is available.
+        """
+        if self._example_pointer == self._num_examples:
+            return None
+        path_to_image_file = self._path_to_image_files[self._example_pointer]
+        index = int(path_to_image_file.split('/')[-1].split('.')[0]) - 1
+        self._example_pointer += 1
+
+        attrs = ExampleReader._get_attrs_for_csv(digit_struct_csv_file, index, csvList)
+        label_of_digits = attrs['label']
+        length = len(label_of_digits)
+        if length > 5:
+            # skip this example
+            return self.read_and_convert_for_csv(digit_struct_csv_file, csvList)
+
+        digits = [10, 10, 10, 10, 10]   # digit 10 represents no digit
+        for idx, label_of_digit in enumerate(label_of_digits):
+            digits[idx] = int(label_of_digit if label_of_digit != 10 else 0)    # label 10 is essentially digit zero
+
+
+        attrs_left, attrs_top, attrs_width, attrs_height = map(lambda x: [int(i) for i in x], [attrs['left'], attrs['top'], attrs['width'], attrs['height']])
+        min_left, min_top, max_right, max_bottom = (min(attrs_left),
+                                                    min(attrs_top),
+                                                    max(map(lambda x, y: x + y, attrs_left, attrs_width)),
+                                                    max(map(lambda x, y: x + y, attrs_top, attrs_height)))
+        center_x, center_y, max_side = ((min_left + max_right) / 2.0,
+                                        (min_top + max_bottom) / 2.0,
+                                        max(max_right - min_left, max_bottom - min_top))
+        bbox_left, bbox_top, bbox_width, bbox_height = (center_x - max_side / 2.0,
+                                                        center_y - max_side / 2.0,
+                                                        max_side,
+                                                        max_side)
+        image = np.array(ExampleReader._preprocess(Image.open(path_to_image_file), bbox_left, bbox_top, bbox_width, bbox_height)).tobytes()
+
+
 
         example = tf.train.Example(features=tf.train.Features(feature={
             'image': ExampleReader._bytes_feature(image),
@@ -106,16 +265,19 @@ def convert_to_tfrecords(path_to_dataset_dir_and_digit_struct_mat_file_tuples,
         num_examples.append(0)
         writers.append(tf.python_io.TFRecordWriter(path_to_tfrecords_file))
 
+
     for path_to_dataset_dir, path_to_digit_struct_mat_file in path_to_dataset_dir_and_digit_struct_mat_file_tuples:
         path_to_image_files = tf.gfile.Glob(os.path.join(path_to_dataset_dir, '*.png'))
         total_files = len(path_to_image_files)
         print '%d files found in %s' % (total_files, path_to_dataset_dir)
+
 
         with h5py.File(path_to_digit_struct_mat_file, 'r') as digit_struct_mat_file:
             example_reader = ExampleReader(path_to_image_files)
             for index, path_to_image_file in enumerate(path_to_image_files):
                 print '(%d/%d) processing %s' % (index + 1, total_files, path_to_image_file)
 
+                # mat 파일의 각종 속성정보(이미지의  각각에 해당하는 좌표정보)를 불러와 example 형태로 다음 내용 저장 : "digits", "image", length"
                 example = example_reader.read_and_convert(digit_struct_mat_file)
                 if example is None:
                     break
@@ -124,8 +286,50 @@ def convert_to_tfrecords(path_to_dataset_dir_and_digit_struct_mat_file_tuples,
                 writers[idx].write(example.SerializeToString())
                 num_examples[idx] += 1
 
+    ############################################################
+    # 폴더 위치가 ./extra 이면, 해당 루틴 실행(user_train)
+    if (path_to_dataset_dir == './data/extra'):
+        # Step 3
+        # 이미지 폴더 경로(./data/user_train)에서 이미지 파일, 갯수 및 csv 파일 로딩
+        path_to_user_train_dir = './data/user_train'
+        path_to_image_files = tf.gfile.Glob(os.path.join(path_to_user_train_dir, '*.png'))
+        total_files = len(path_to_image_files)
+        print '%d files found in %s' % (total_files, path_to_user_train_dir)
+
+        with open('./data/user_train/user_train.csv', 'r') as csvfile:
+
+            # csv 파일에서 각각의 line을 csvList 형태로 누적 로드(이미지 관련 info)
+            ############################################################
+            csvList = {}
+            line_tmp = []
+
+            csv_reader = csv.DictReader(csvfile)
+
+            for line in csv_reader:
+                line_tmp.append(line)
+                csvList = line_tmp
+            ############################################################
+
+            example_reader = ExampleReader(path_to_image_files)
+            for index, path_to_image_file in enumerate(path_to_image_files):
+                print '(%d/%d) processing %s' % (index + 1, total_files, path_to_image_file)
+
+                # mat 파일의 각종 속성정보(이미지의  각각에 해당하는 좌표정보)를 불러와 example 형태로 다음 내용 저장 : "digits", "image", length"
+                example = example_reader.read_and_convert_for_csv(csvfile, csvList)
+                if example is None:
+                    break
+
+                idx = choose_writer_callback(path_to_tfrecords_files)
+                writers[idx].write(example.SerializeToString())
+                num_examples[idx] += 1
+
+
+    else :
+        pass
+            ############################################################
+
     for writer in writers:
-        writer.close()
+        writer.close() # write 후 닫기
 
     return num_examples
 
@@ -140,37 +344,65 @@ def create_tfrecords_meta_file(num_train_examples, num_val_examples, num_test_ex
     meta.save(path_to_tfrecords_meta_file)
 
 
-def main(_):
-    path_to_train_dir = os.path.join(FLAGS.data_dir, 'train')
-    path_to_test_dir = os.path.join(FLAGS.data_dir, 'test')
-    path_to_extra_dir = os.path.join(FLAGS.data_dir, 'extra')
-    path_to_train_digit_struct_mat_file = os.path.join(path_to_train_dir, 'digitStruct.mat')
-    path_to_test_digit_struct_mat_file = os.path.join(path_to_test_dir, 'digitStruct.mat')
-    path_to_extra_digit_struct_mat_file = os.path.join(path_to_extra_dir, 'digitStruct.mat')
+def main_convert_to_tfrecords(_):
 
-    path_to_train_tfrecords_file = os.path.join(FLAGS.data_dir, 'train.tfrecords')
-    path_to_val_tfrecords_file = os.path.join(FLAGS.data_dir, 'val.tfrecords')
-    path_to_test_tfrecords_file = os.path.join(FLAGS.data_dir, 'test.tfrecords')
-    path_to_tfrecords_meta_file = os.path.join(FLAGS.data_dir, 'meta.json')
+    # 기존 tfrecords 파일 제거(디버깅시 사용)
+    # if os.path.isfile('./data/train.tfrecords'):
+    #     os.remove('./data/train.tfrecords')
+    # if os.path.isfile('./data/val.tfrecords'):
+    #     os.remove('./data/val.tfrecords')
+    # if os.path.isfile('./data/test.tfrecords'):
+    #     os.remove('./data/test.tfrecords')
 
-    for path_to_file in [path_to_train_tfrecords_file, path_to_val_tfrecords_file, path_to_test_tfrecords_file]:
+
+    parser = argparse.ArgumentParser(description="Convert to tfrecords foramt for SVHNClassifier")
+    parser.add_argument("--data_dir", required=True, help="Directory to SVHN (format 1) folders and write the converted files")
+    parser.add_argument("--path_to_train_dir", required=True, help="Directory for train data")
+    parser.add_argument("--path_to_test_dir", required=True, help="Directory for test data")
+    parser.add_argument("--path_to_extra_dir", required=True, help="Directory for extra data")
+    parser.add_argument("--path_to_user_train_dir", required=True, help="Directory for user train data")
+    parser.add_argument("--path_to_train_digit_struct_mat_file", required=True, help="Digit struct mat file in train directory")
+    parser.add_argument("--path_to_test_digit_struct_mat_file", required=True, help="Digit struct mat file in test directory")
+    parser.add_argument("--path_to_extra_digit_struct_mat_file", required=True, help="Digit struct mat file in extra directory")
+    parser.add_argument("--path_to_train_tfrecords_file", required=True, help="Tfrecords file in train directory")
+    parser.add_argument("--path_to_val_tfrecords_file", required=True, help="Tfrecords file in val directory")
+    parser.add_argument("--path_to_test_tfrecords_file", required=True, help="Tfrecords file in test directory")
+    parser.add_argument("--path_to_tfrecords_meta_file", required=True, help="Tfrecords meta file")
+    parser.add_argument("--in_csv_file", required=True, help="User train csv file in user_train directory")
+    args = parser.parse_args()
+
+
+
+    for path_to_file in [args.path_to_train_tfrecords_file, args.path_to_val_tfrecords_file, args.path_to_test_tfrecords_file]:
         assert not os.path.exists(path_to_file), 'The file %s already exists' % path_to_file
 
     print 'Processing training and validation data...'
-    [num_train_examples, num_val_examples] = convert_to_tfrecords([(path_to_train_dir, path_to_train_digit_struct_mat_file),
-                                                                   (path_to_extra_dir, path_to_extra_digit_struct_mat_file)],
-                                                                  [path_to_train_tfrecords_file, path_to_val_tfrecords_file],
+    [num_train_examples, num_val_examples] = convert_to_tfrecords([(args.path_to_train_dir, args.path_to_train_digit_struct_mat_file),
+                                                                   (args.path_to_extra_dir, args.path_to_extra_digit_struct_mat_file)],
+                                                                  [args.path_to_train_tfrecords_file, args.path_to_val_tfrecords_file],
                                                                   lambda paths: 0 if random.random() > 0.1 else 1)
+
     print 'Processing test data...'
-    [num_test_examples] = convert_to_tfrecords([(path_to_test_dir, path_to_test_digit_struct_mat_file)],
-                                               [path_to_test_tfrecords_file],
+    [num_test_examples] = convert_to_tfrecords([(args.path_to_test_dir, args.path_to_test_digit_struct_mat_file)],
+                                               [args.path_to_test_tfrecords_file],
                                                lambda paths: 0)
 
     create_tfrecords_meta_file(num_train_examples, num_val_examples, num_test_examples,
-                               path_to_tfrecords_meta_file)
+                               args.path_to_tfrecords_meta_file)
 
     print 'Done'
 
 
 if __name__ == '__main__':
-    tf.app.run(main=main)
+
+    if len(sys.argv) == 1:
+
+        sys.argv.extend(["--data_dir",  "./data",                                               "--path_to_train_dir", "./data/train",
+                         "--path_to_test_dir", "./data/test",                                   "--path_to_extra_dir", "./data/extra",
+                         "--path_to_user_train_dir", "./data/user_train",                       "--path_to_train_digit_struct_mat_file", "./data/train/digitStruct.mat",
+                         "--path_to_test_digit_struct_mat_file", "./data/test/digitStruct.mat", "--path_to_extra_digit_struct_mat_file", "./data/extra/digitStruct.mat",
+                         "--path_to_train_tfrecords_file", "./data/train.tfrecords",            "--path_to_val_tfrecords_file", "./data/val.tfrecords",
+                         "--path_to_test_tfrecords_file", "./data/test.tfrecords",              "--path_to_tfrecords_meta_file", "./data/meta.json",
+                         "--in_csv_file", "./data/user_train/user_train.csv"])
+
+    tf.app.run(main=main_convert_to_tfrecords)
